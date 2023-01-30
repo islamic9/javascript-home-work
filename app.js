03927230a23af7d0e317beed64cdbd4f7d7e2033
islamic9 <gitbushprofile01@gmail.com>
2023-01-30 17:57:29 +0500

    javascript home work

---
+++ app.js
@@ -0,0 +1,14 @@
+const height = +prompt("balandlik qiymatini sonlar orqali kiriting");
+const width = +prompt("eni qiymatini sonlar orqali kiriting");
+const x = (width + "rem")
+const y = (height + "rem")
+const yuzasi = height * width;
+const formula =2*(height + width);
+document.getElementById("tortburchak").style.border  = "solid 5px";
+document.getElementById("tortburchak").style.maxWidth  = "auto";
+document.getElementById("tortburchak").style.minHeight  = "auto";
+document.getElementById("tortburchak").style.background = "gold";
+document.getElementById("tortburchak").style.width = x;
+document.getElementById("tortburchak").style.height = y;
+
+  
